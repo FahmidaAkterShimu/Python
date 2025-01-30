687cd3834575adbbffc5355c37ec2b191828b06d
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2025-01-31 00:22:05 +0600

    Add questions

---
+++ Chapter 1/Problem2.py
@@ -0,0 +1,3 @@
+# Use REPL and print the table of 5 using Python  
+for i in range(1, 11):
+    print(f"5 x {i} = {5 * i}")
---
+++ first.py
@@ -0,0 +1,10 @@
+#demo
+print("Hello World")
+
+#module
+import pyjokes
+"""
+This prints a random jokes
+"""
+joke = pyjokes.get_joke()
+print(joke)
