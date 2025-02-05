3a58b7e55b43c073081cd0823cdb636627eeddfe
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2025-02-05 22:54:48 +0600

    Add in Chapter 3

---
+++ Chapter 3/3.4_Escape Sequence.py
@@ -0,0 +1,11 @@
+a = "I need a new line\nin my writing."
+print(a)
+
+b = "I need a \t tab space"
+print(b)
+
+c = "When I need \"Double\" quote."
+print(c)
+
+d = 'When I need \'Single\' quote.'
+print(d)
