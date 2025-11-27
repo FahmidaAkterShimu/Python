19f39040d6d42a7e4cd8a872ce86e526892c7f7e
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2025-11-27 19:35:03 +0600

    Level 6

--- Level 6/6.1_conditionals.py
+++ Level 6/6.1_conditionals.py
@@ -1,6 +1,6 @@
 a = int(input("Enter your age: "))
 
 if(a>18):
-    print("You are above the age of concent")
+    print("You are above the age of consent")
 else:
-    print("You are below the age of concent")
+    print("You are below the age of consent")
