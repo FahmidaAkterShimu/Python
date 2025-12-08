95ff1615b67dc62f0462e725ed2de346cd6de44e
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2025-12-08 13:17:22 +0600

    Level 7

--- Level 7/Problem7.py
+++ Level 7/Problem7.py
@@ -5,6 +5,9 @@
 
 n = int(input("Enter the number: "))
 for i in range(1, n+1):
-    print(" "* (n-i), end="")
-    print("*"* (2*i-1), end="")
-    print("\n")
+    print(" " * (n-i), "*" * (2*i-1))
+
+for i in range(1, n+1):
+    print(" " * (n-i), end="" )
+    print("*" * (2*i-1), end="")
+    print("")
