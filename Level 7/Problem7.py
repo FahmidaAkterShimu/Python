d5678b188dbc14846e0d680a41267b4fc12cbcc9
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2025-12-08 16:31:31 +0600

    Level 7

--- Level 7/Problem7.py
+++ Level 7/Problem7.py
@@ -8,7 +8,9 @@ for i in range(1, n+1):
     print(" " * (n-i), "*" * (2*i-1))
 
 # Or,
+'''
 for i in range(1, n+1):
     print(" " * (n-i), end="" )
     print("*" * (2*i-1), end="")
     print("")
+'''
