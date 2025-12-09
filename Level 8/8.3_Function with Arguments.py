1dbe16965fe8f98ccb5a780a57c9ff5806e50138
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2025-12-09 21:34:32 +0600

    Level 8

---
+++ Level 8/8.3_Function with Arguments.py
@@ -0,0 +1,8 @@
+def goodDay(name, ending):
+    print("Good Day! " + name)
+    print(ending)
+
+goodDay("Shimu", "Thank you")
+goodDay("Shova", "Thank you")
+goodDay("Smita", "Thanks")
+goodDay("Emu", "Thank you")
