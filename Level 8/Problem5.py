d4eb1170e5b4d7952c8e04773b97d17ac979ce43
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-01-04 16:54:32 +0600

    Problem5

---
+++ Level 8/Problem5.py
@@ -0,0 +1,14 @@
+# Write a python function to print first n lines of the following pattern: 
+# *** 
+# **               
+# *              for n = 3
+
+def pattern(n):
+    if (n==0):
+        return 0
+        
+    print("*" * n)
+    pattern(n-1)
+
+n = int(input("Enter a number: "))
+pattern(n)
